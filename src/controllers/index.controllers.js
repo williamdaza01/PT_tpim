473be02cb2771300a830b502ee81a33b11684087
Daza <williamdaza04@gmail.com>
2022-09-30 11:28:55 -0500

    Se agrega autorizacion para urls

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,137 +1,204 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: 'localhost',
-    user: "postgres",
-    password: "lt123456",
-    database: "postgres",
-    port: 5432
-})
+  host: "localhost",
+  user: "postgres",
+  password: "lt123456",
+  database: "postgres",
+  port: 5432,
+});
 
 //GET
-const getSales = async(req,res) => {
-    const response = await pool.query('SELECT * FROM "Db_Store".sales');
-    res.status(200).json(response.rows);
-}
+const getSales = async (req, res) => {
+  const response = await pool.query('SELECT * FROM "Db_Store".sales');
+  res.status(200).json(response.rows);
+};
 
-const getProducts = async(req,res) => {
-    const response = await pool.query('SELECT * FROM "Db_Store".products');
-    res.status(200).json(response.rows);
-}
+const getProducts = async (req, res) => {
+  const response = await pool.query('SELECT * FROM "Db_Store".products');
+  res.status(200).json(response.rows);
+};
 
-const getUsers = async(req,res) => {
-    const response = await pool.query('SELECT * FROM "Db_Store".users');
-    res.status(200).json(response.rows);
-}
+const getUsers = async (req, res) => {
+  const response = await pool.query('SELECT * FROM "Db_Store".users');
+  res.status(200).json(response.rows);
+};
 
-const getSalesDay = async (req,res) => {
-    const date = req.params.date;
-    console.log(date);
+const getSalesDay = async (req, res) => {
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const user_id = req.params.roleid;
+  const date = req.params.date;
+  if (user_id !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
     const response = await pool.query(`select count(*) from "Db_Store".sales
-    where sales_at = date('${date}');`);
+                                where sales_at = date('${date}');`);
     res.status(200).json(response.rows);
-}
+  }
+};
 
-const getSalesMonth = async (req,res) => {
-    const date = req.params.sales_at;
-    const response = await pool.query(`select sum(extract('month', '${date}')) as date
+const getSalesMonth = async (req, res) => {
+  const date = req.params.sales_at;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const user_id = req.params.roleid;
+  if (user_id !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response =
+      await pool.query(`select sum(extract('month', '${date}')) as date
     from "Db_Store".sales s;`);
     res.status(200).json(response.rows);
-}
+  }
+};
 
 //POST
-const postSales = async(req,res) => {
- //console.log(req);
-    const {qty, sales_at, product_id, user_id} = req.body;
-    console.log(product_id);
-    const response = await pool.query(`INSERT INTO "Db_Store".sales(qty, sales_at, product_id, user_id) 
+const postSales = async (req, res) => {
+  const { qty, sales_at, product_id, user_id } = req.body;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response =
+      await pool.query(`INSERT INTO "Db_Store".sales(qty, sales_at, product_id, user_id) 
                                         VALUES(${qty}, '${sales_at}', '${product_id}', '${user_id}');`);
     res.json({
-        message: "Venta creada",
-        body: {
-            sale: {qty, sales_at, product_id, user_id}
-        }
+      message: "Venta creada",
+      body: {
+        sale: { qty, sales_at, product_id, user_id },
+      },
     });
-}
+  }
+};
 
-const postProduct = async(req,res) => {
-    const {description, name, price} = req.body;
-    const response = await pool.query(`INSERT INTO "Db_Store".products(description, name, price)
+const postProduct = async (req, res) => {
+  const { description, name, price } = req.body;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response =
+      await pool.query(`INSERT INTO "Db_Store".products(description, name, price)
                                        VALUE('${description}', '${name}', ${price});`);
     res.json({
-        message: "Producto creado",
-        body:{
-            sale: {description, name, price}
-        }
+      message: "Producto creado",
+      body: {
+        sale: { description, name, price },
+      },
     });
-}
+  }
+};
 
-const postUser = async(req,res) => {
-    const {name, last_name, document, role_id} = req.body;
-    const response = await pool.query(`INSERT INTO "Db_Store".users(name, last_name, document, role_id)
+const postUser = async (req, res) => {
+  const { name, last_name, document, role_id } = req.body;
+  const roleId = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== roleId) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response =
+      await pool.query(`INSERT INTO "Db_Store".users(name, last_name, document, role_id)
                                        VALUE('${name}', '${last_name}', '${document}', '${role_id}');`);
     res.json({
-        message: "Usuario creado",
-        body:{
-            sale: {name, last_name, document, role_id}
-        }
+      message: "Usuario creado",
+      body: {
+        sale: { name, last_name, document, role_id },
+      },
     });
-}
+  }
+};
 
-const postRole = async(req,res) => {
-    const {name} = req.body;
+const postRole = async (req, res) => {
+  const { name } = req.body;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
     const response = await pool.query(`INSERT INTO "Db_Store".users(name)
                                        VALUE('${name}');`);
     res.json({
-        message: "Rol creado",
-        body:{
-            sale: {name}
-        }
+      message: "Rol creado",
+      body: {
+        sale: { name },
+      },
     });
-}
+  }
+};
 
 //PUT
-const putSale = async (req,res) => {
-    const id = req.params.id;
-    const {qty, sales_at} = req.body;
-    const response = await pool.query(`update "Db_Store".sales set qty = ${qty} , sales_at = '${sales_at}' 
+const putSale = async (req, res) => {
+  const id = req.params.id;
+  const { qty, sales_at } = req.body;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response =
+      await pool.query(`update "Db_Store".sales set qty = ${qty} , sales_at = '${sales_at}' 
                                       where id= '${id}'`);
     res.json("Venta actualizada con exito");
-}
+  }
+};
 
-const putRole = async (req,res) => {
-    const id = req.params.id;
-    const {role_id} = req.body;
-    const response = await pool.query(`update "Db_Store".users set role_id = '${role_id}'
+const putRole = async (req, res) => {
+  const id = req.params.id;
+  const { role_id } = req.body;
+  const roleId = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== roleId) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response =
+      await pool.query(`update "Db_Store".users set role_id = '${role_id}'
                                       where id= '${id}'`);
     res.json("Rol actualizado con exito");
-}
+  }
+};
 
 //DELETE
-const deleteSale = async (req,res) => {
-    const id = req.params.id;
-    const response = await pool.query(`delete from "Db_Store".sales where '${id}'`);
+const deleteSale = async (req, res) => {
+  const id = req.params.id;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response = await pool.query(
+      `delete from "Db_Store".sales where '${id}'`
+    );
     res.json("venta eliminada con exito");
-}
+  }
+};
 
-const deleteUser = async (req,res) => {
-    const id = req.params.id;
-    const response = await pool.query(`delete from "Db_Store".users where '${id}'`);
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const role_id = "'ad674a74-0d44-445e-b697-7b9103bb705c'";
+  const useridrole = req.params.roleid;
+  if (useridrole !== role_id) {
+    res.status(401).json("Acceso denegado");
+  } else {
+    const response = await pool.query(
+      `delete from "Db_Store".users where '${id}'`
+    );
     res.json("Usuario eliminado con exito");
-}
+  }
+};
 
 module.exports = {
-    getSales,
-    getProducts,
-    getUsers,
-    getSalesDay,
-    getSalesMonth,
-    postProduct,
-    postRole,
-    postSales,
-    postUser, 
-    putRole,
-    putSale,
-    deleteSale,
-    deleteUser
-}
+  getSales,
+  getProducts,
+  getUsers,
+  getSalesDay,
+  getSalesMonth,
+  postProduct,
+  postRole,
+  postSales,
+  postUser,
+  putRole,
+  putSale,
+  deleteSale,
+  deleteUser,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,16 +10,16 @@ const {
 
 router.get('/users', getUsers);
 router.get('/sales', getSales);
-router.get('/sales/:date', getSalesDay);
-router.get('/sales/salesXmonth/:date', getSalesMonth);
+router.get('/sales/:date/:roleid', getSalesDay);
+router.get('/salesXmonth/:date/:roleid', getSalesMonth);
 router.get('/products', getProducts);
-router.post('/users', postUser);
-router.post('/sales', postSales);
-router.post('/products', postProduct);
-router.post('/roles', postRole);
-router.put('/users/:id', putRole);
-router.put('/sales/:id', putSale);
-router.delete('/sales/:id', deleteSale);
-router.delete('/users/:id', deleteUser);
+router.post('/users/:roleid', postUser);
+router.post('/sales/:roleid', postSales);
+router.post('/products/:roleid', postProduct);
+router.post('/roles/:roleid', postRole);
+router.put('/users/:id/:roleid', putRole);
+router.put('/sales/:id/:roleid', putSale);
+router.delete('/sales/:id/:roleid', deleteSale);
+router.delete('/users/:id/:roleid', deleteUser);
 
 module.exports = router;

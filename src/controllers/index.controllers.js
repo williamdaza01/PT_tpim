fc2cc563188afdee24c7f10155499d357653d676
Daza <williamdaza04@gmail.com>
2022-09-29 23:53:29 -0500

    Se realizan peticiones get

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -8,3 +8,44 @@ const pool = new Pool({
     port: 5432
 })
 
+//GET
+const getSales = async(req,res) => {
+    const response = await pool.query('SELECT * FROM "Db_Store".sales');
+    res.status(200).json(response.rows);
+}
+
+const getProducts = async(req,res) => {
+    const response = await pool.query('SELECT * FROM "Db_Store".products');
+    res.status(200).json(response.rows);
+}
+
+const getUsers = async(req,res) => {
+    const response = await pool.query('SELECT * FROM "Db_Store".users');
+    res.status(200).json(response.rows);
+}
+
+const getSalesDay = async (req,res) => {
+    const date = req.params.date;
+    console.log(date);
+    const response = await pool.query(`select count(*) from "Db_Store".sales
+    where sales_at = date('${date}');`);
+    res.status(200).json(response.rows);
+}
+
+const getSalesMonth = async (req,res) => {
+    const date = req.params.sales_at;
+    const response = await pool.query(`select count(s.id), date_trunc('month', '${date}') as date
+    from "Db_Store".sales s
+    group by date;`);
+    res.status(200).json(response.rows);
+}
+
+
+
+module.exports = {
+    getSales,
+    getProducts,
+    getUsers,
+    getSalesDay,
+    getSalesMonth,
+}
